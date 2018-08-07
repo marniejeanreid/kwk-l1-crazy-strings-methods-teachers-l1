518b481a15b3401a0ecb50aecf4b15ec83f14ee1
Marnie Reid <marniejeanreid@gmail.com>
2018-08-07 14:23:28 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -3,7 +3,7 @@
 
 def crazy_strings(first_string, second_string)
   first_string = first_string.reverse.upcase
-  second_string = second_string.swapcase.sub!("s", "z")
+  second_string = second_string.swapcase.gsub("s", "z")
   return "#{first_string} #{second_string}"
 end
 
